5c37135aab4adb85582729fcb89c8de1f027b368
brian-tura <b.tura2002@gmail.com>
2025-03-25 08:43:40 +0100

    Excercise 2 done

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function firstLetter(names) {
+    const nameInitals = [];
+    var holder;
+    for(var i = 0; i < names.length; i++){
+        holder = names[i];
+        nameInitals.push(holder.charAt(0));
+    }
+    return nameInitals;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(firstLetter(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
