b9b3908eb644c9a0d741f52edcd01e61578af678
brian-tura <b.tura2002@gmail.com>
2025-03-25 09:54:21 +0100

    Last Excercise Done

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function isThereVowels(word){
+    let v = 0;
+    const check = ["a", "e", "i", "o", "u"];
+    for(var i = 0; i < word.length; i++){
+        for(var j = 0; j < check.length; j++){
+            if(word.charAt(i) === check[j]){
+                v += 1;
+            }
+        }
+    }
+    return v;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(isThereVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
