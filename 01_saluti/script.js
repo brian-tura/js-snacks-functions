28a779f4a9413377f771905c84b3a761c9afce3e
brian-tura <b.tura2002@gmail.com>
2025-03-25 08:27:14 +0100

    Excercise 1 done

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,10 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+hello = (userName) => "Ciao " + userName;
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(hello(userName));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
